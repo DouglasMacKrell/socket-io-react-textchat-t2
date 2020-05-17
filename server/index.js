52ba4ca374d1e00a9d8afeb6b30b87c75ae13ba4
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-05-16 21:34:43 -0400

    3 4ths complete

--- server/index.js
+++ server/index.js
@@ -17,7 +17,24 @@ io.on('connection', (socket) => {
 
     socket.on('join', ({ name, room }, callback) => {
         console.log(name, room);
-        
+        const { error, user } = addUser({ id: socket.id, name, room });
+
+        if(error) return callback(error);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room, ${user.room}` });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined the room!` });
+
+        socket.join(user.room);
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message });
+
+        callback();
     })
 
     socket.on('disconnect', () => {
--- server/users.js
+++ server/users.js
@@ -2,8 +2,8 @@ const users = [];
 
 const addUser = ({ id, name, room }) => {
     // Douglas MacKrell = douglasmackrell
-    name = name.trim().toLowercase();
-    room = room.trim().toLowercase();
+    name = name.trim().toLowerCase();
+    room = room.trim().toLowerCase();
 
     const existingUser = users.find((user) => user.room === room && user.name === name);
     if(existingUser) {
