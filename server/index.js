385e205292c82df10e1dbc282c40cc0aa3d45158
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-05-16 20:09:59 -0400

    user helper functions added

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log("We have a new connection!!!");
+
+    socket.on('join', ({ name, room }, callback) => {
+        console.log(name, room);
+        
+    })
+
+    socket.on('disconnect', () => {
+        console.log("User has left the connection!!!");
+    });
+});
+
+app.use(router)
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+
